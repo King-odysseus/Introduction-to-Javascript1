37eef10f8fed02520961e434ca1b429732187e86
king-odysseus <wingzatari@gmail.com>
2022-06-21 15:21:33 +0100

    first commit

--- index.js
+++ index.js
@@ -2,4 +2,4 @@ let name = "Nathaniel Jemine Boyo"
 let height = "5ft 9 inches tall" 
 let country = "Nigeria" 
 
-console.log( name  + ", "  + height + ",  " + country );
+console.log( name  + ", "  + height + ",  " + country + ".");
